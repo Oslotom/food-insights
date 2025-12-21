2bcdb2cc7902bb3577911280a280c49a848c09e3
Oslotom <94135366+Oslotom@users.noreply.github.com>
2025-12-21 02:17:53 +0100

    asdfsadfasdf

--- vite.config.ts
+++ vite.config.ts
@@ -5,7 +5,7 @@ import { componentTagger } from "lovable-tagger";
 
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
-  base: "/food-insights/",
+  base: mode === 'production' ? "/food-insights/" : "/",
   server: {
     host: "0.0.0.0",
     port: 5000,
